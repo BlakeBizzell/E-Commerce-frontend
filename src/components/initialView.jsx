f6378e1215a1867a4612a5515a485b7ea5966d94
preston <polston13@outlook.com>
2024-02-13 17:11:48 -0500

    finished image carousel

--- src/components/initialView.jsx
+++ src/components/initialView.jsx
@@ -4,6 +4,9 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
+import Carousel from "react-material-ui-carousel";
+import { useEffect, useState } from "react";
+import { useGetProductsQuery } from "../api/soapApi";
 
 const bull = (
   <Box
@@ -14,57 +17,92 @@ const bull = (
   </Box>
 );
 
-export default function InitialView() {
+const InitialView = () => {
+  const [products, setProducts] = useState([]);
+  const { data: fetchedData, error, isLoading } = useGetProductsQuery();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!isLoading && !error) {
+        setProducts(fetchedData);
+      }
+    };
+
+    fetchData();
+  }, [fetchedData, isLoading, error]);
+
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      <Card sx={{ width: 400, height: 300 }}>
-        <CardContent>
-          <CardActions
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              height: "100%",
-            }}
-          >
-            <Link to="/sign-in">
-              <Button
-                sx={{ margin: "10px 0" }}
-                variant="contained"
-                size="large"
-              >
-                Login
-              </Button>
-            </Link>
-            <Link to="/sign-up">
-              <Button
-                sx={{ margin: "10px 0" }}
-                variant="contained"
-                size="large"
-              >
-                Sign up
-              </Button>
-            </Link>
-            <Link to="/products">
-              <Button
-                sx={{ margin: "10px 0" }}
-                variant="contained"
-                size="large"
-              >
-                Continue as guest
-              </Button>
-            </Link>
-          </CardActions>
-        </CardContent>
-      </Card>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div>
+        <Carousel>
+          {products.map((product) => (
+            <img
+              key={product.id}
+              src={`data:image/png;base64, ${product.image}`}
+              alt="Product Image"
+              style={{
+                height: "400px",
+                display: "block",
+                margin: "0 auto",
+              }}
+            />
+          ))}
+        </Carousel>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <Card sx={{ width: 400, height: 300 }}>
+          <CardContent>
+            <CardActions
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100%",
+              }}
+            >
+              <Link to="/sign-in">
+                <Button
+                  sx={{ margin: "10px 0" }}
+                  variant="contained"
+                  size="large"
+                >
+                  Login
+                </Button>
+              </Link>
+              <Link to="/sign-up">
+                <Button
+                  sx={{ margin: "10px 0" }}
+                  variant="contained"
+                  size="large"
+                >
+                  Sign up
+                </Button>
+              </Link>
+              <Link to="/guest">
+                <Button
+                  sx={{ margin: "10px 0" }}
+                  variant="contained"
+                  size="large"
+                >
+                  Continue as guest
+                </Button>
+              </Link>
+            </CardActions>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
-}
+};
+
+export default InitialView;
