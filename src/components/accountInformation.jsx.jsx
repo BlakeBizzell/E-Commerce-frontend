144acfdbe42c8868c172f75abe0a38959963f851
preston <polston13@outlook.com>
2024-02-19 17:38:34 -0500

    getUser is functional needs css

--- src/components/accountInformation.jsx.jsx
+++ src/components/accountInformation.jsx.jsx
@@ -1,4 +1,32 @@
+import { useGetUserQuery } from "../api/soapApi";
+import { Box, Card, CardContent } from "@mui/material";
+import { useSelector } from "react-redux";
 const GetAccount = () => {
-  return <p>this is your account information</p>;
+  const userId = useSelector((state) => state.user.id);
+  const { data, isError, isLoading, isSuccess } = useGetUserQuery(userId);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p>Error loading user data</p>;
+  }
+
+  if (isSuccess) {
+    return (
+      <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+        <Card key={data.id} sx={{ minWidth: 275, maxWidth: 400, m: 1 }}>
+          <CardContent>
+            <h1>{data.username}</h1>
+            <h1>{data.firstName}</h1>
+            <h1>{data.lastName}</h1>
+            <h1>{data.email}</h1>
+          </CardContent>
+        </Card>
+      </Box>
+    );
+  }
 };
+
 export default GetAccount;
