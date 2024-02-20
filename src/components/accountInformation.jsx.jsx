8e6bde3bc36b070a0a31fcfd1b51ed1472ad31cc
pradora <prado399@gmail.com>
2024-02-20 16:15:44 -0500

    changed stylized accountInformation.jsx

--- src/components/accountInformation.jsx.jsx
+++ src/components/accountInformation.jsx.jsx
@@ -16,13 +16,17 @@ const GetAccount = () => {
 
   if (isSuccess) {
     return (
-      <Box sx={{ display: "flex", flexWrap: "wrap" }}>
-        <Card key={data.id} sx={{ minWidth: 275, maxWidth: 400, m: 1 }}>
+      <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center", alignItems: "center", minHeight: "100vh"  }}>
+        <Card key={data.id} sx={{width: "20%",height: "30%", m: 1 }}>
           <CardContent>
-            <h1>{data.username}</h1>
-            <h1>{data.firstName}</h1>
-            <h1>{data.lastName}</h1>
-            <h1>{data.email}</h1>
+            <h4 style={{ margin: "0" }}>User Name:</h4>
+            <p style={{ margin: "0", marginBottom: "10px" }}>{data.username}</p>
+            <h4 style={{ margin: "0" }}>First Name:</h4>
+            <p style={{ margin: "0", marginBottom: "10px" }}>{data.firstName}</p>
+            <h4 style={{ margin: "0" }}>Last Name:</h4>
+            <p style={{ margin: "0", marginBottom: "10px" }}>{data.lastName}</p>
+            <h4 style={{ margin: "0" }}>Email:</h4>
+            <p style={{ margin: "0", marginBottom: "10px" }}>{data.email}</p>
             <Link to={"/updateUserInfo"}>
               <Button>Update Information</Button>
             </Link>
